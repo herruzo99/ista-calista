29c294dd8ea7db2179681f864ef4ae7bd953a729
Juan Herruzo <juanherruzo@gmail.com>
2024-12-01 13:46:46 +0100

    working version v1

--- ista-calista/__init__.py
+++ ista-calista/__init__.py
@@ -4,21 +4,22 @@ from __future__ import annotations
 
 import logging
 
-from .pycalista_ista import LoginError, PyCalistaIsta, ServerError
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall, ServiceResponse
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
 
 from .const import DOMAIN
 from .coordinator import IstaCoordinator
+from .pycalista_ista import LoginError, PyCalistaIsta, ServerError
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 type IstaConfigEntry = ConfigEntry[IstaCoordinator]
+
+
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: IstaConfigEntry) -> bool:
@@ -28,25 +29,48 @@ async def async_setup_entry(hass: HomeAssistant, entry: IstaConfigEntry) -> bool
         entry.data[CONF_PASSWORD],
     )
     try:
-        await hass.async_add_executor_job(ista.login)
+        pass #await hass.async_add_executor_job(ista.login)
     except ServerError as e:
         raise ConfigEntryNotReady(
             translation_domain=DOMAIN,
             translation_key="connection_exception",
         ) from e
-    except (LoginError) as e:
+    except LoginError as e:
         raise ConfigEntryAuthFailed(
             translation_domain=DOMAIN,
             translation_key="authentication_exception",
             translation_placeholders={CONF_EMAIL: entry.data[CONF_EMAIL]},
         ) from e
-
     coordinator = IstaCoordinator(hass, ista)
     await coordinator.async_config_entry_first_refresh()
 
     entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    # async def __back_fill_data(
+    #     call: ServiceCall
+    # ) -> ServiceResponse:
+    #     """Get history data from ista Calista."""
+    #     _LOGGER.warning("__back_fill_data")
+    #     entities = [
+    #         entity
+    #         for entity in hass.states.async_all()
+    #         if (entity.entity_id.startswith(DOMAIN + ".") if entity.entity_id else False)
+    #     ]
+
+    #     # Example: Log and update each entity
+    #     for entity in entities:
+    #         _LOGGER.warning(f"Found entity: {entity.entity_id}, State: {entity.state}, Object id: {entity.object_id}")
+    #         # Optionally trigger state updates or handle data
+
+
+    # hass.services.async_register(
+    #     DOMAIN,
+    #     "backfill_long_term_statistics",
+    #     service_func=__back_fill_data,
+    # )
+
 
     return True
 
--- ista-calista/sensor.py
+++ ista-calista/sensor.py
@@ -2,12 +2,11 @@
 
 from __future__ import annotations
 
-import asyncio
+from collections.abc import Callable
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import UTC, datetime, timedelta
 from enum import StrEnum
 import logging
-from typing import Callable, Dict
 
 from homeassistant.components.recorder.models.statistics import (
     StatisticData,
@@ -26,10 +25,7 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.const import UnitOfVolume
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import (
-    DeviceEntry,
-    DeviceInfo,
-)
+from homeassistant.helpers.device_registry import DeviceEntry, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -37,6 +33,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from . import IstaConfigEntry
 from .const import DOMAIN
 from .coordinator import IstaCoordinator
+from .pycalista_ista import Device, HeatingDevice, WaterDevice
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -53,8 +50,9 @@ class IstaConsumptionType(StrEnum):
 class CalistaSensorEntityDescription(SensorEntityDescription):
     """Describes Ista Calista sensor entity."""
 
-    exists_fn: Callable[[Dict[str, any]], bool] = lambda _: True
-    value_fn: Callable[[Dict[str, any]], StateType]
+    exists_fn: Callable[[Device], bool] = lambda _: True
+    value_fn: Callable[[Device], StateType]
+    generate_lts: bool
 
 
 SENSOR_DESCRIPTIONS: tuple[CalistaSensorEntityDescription, ...] = (
@@ -63,20 +61,24 @@ SENSOR_DESCRIPTIONS: tuple[CalistaSensorEntityDescription, ...] = (
         translation_key="water",
         native_unit_of_measurement=UnitOfVolume.CUBIC_METERS,
         device_class=SensorDeviceClass.WATER,
-        state_class=SensorStateClass.TOTAL,
-        value_fn=lambda data: data["lectura_actual"],
-        exists_fn=lambda data: data["tipo_equipo"]
-        != "Radio Distribuidor de Costes de Calefacción",
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        value_fn=lambda device: device.last_reading.reading
+        if device.last_reading
+        else None,
+        exists_fn=lambda device: isinstance(device, WaterDevice),
+        generate_lts = True
     ),
     CalistaSensorEntityDescription(
-        key="relative_heating",
+        key="total_heating",
         translation_key="heating",
-        native_unit_of_measurement=None,
-        device_class=None,
-        state_class=SensorStateClass.TOTAL,
-        value_fn=lambda data: data["lectura_actual"],
-        exists_fn=lambda data: data["tipo_equipo"]
-        == "Radio Distribuidor de Costes de Calefacción",
+        native_unit_of_measurement=UnitOfVolume.CUBIC_METERS,
+        device_class=SensorDeviceClass.GAS,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        value_fn=lambda device: device.last_reading.reading
+        if device.last_reading
+        else None,
+        exists_fn=lambda device: isinstance(device, HeatingDevice),
+        generate_lts = True
     ),
     CalistaSensorEntityDescription(
         key="last_reading",
@@ -84,8 +86,11 @@ SENSOR_DESCRIPTIONS: tuple[CalistaSensorEntityDescription, ...] = (
         native_unit_of_measurement=None,
         device_class=SensorDeviceClass.DATE,
         state_class=None,
-        value_fn=lambda data: datetime.strptime(data["fecha"], "%d/%m/%Y"),
-        exists_fn=lambda data: True,
+        value_fn=lambda device: device.last_reading.date
+        if device.last_reading
+        else None,
+        exists_fn=lambda device: isinstance(device, Device),
+        generate_lts = False
     ),
 )
 
@@ -96,29 +101,20 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the ista Calista sensors."""
-
+    _LOGGER.error('called async_setup_entry')
     coordinator = config_entry.runtime_data
 
-    # Check if coordinator is ready and data is available
-    if coordinator.data is None:
-        # If data is not available, wait for it asynchronously
-        await coordinator.async_refresh()
-        # Ensure the data is populated after refresh
-        if coordinator.data is None:
-            return False  # No data available, so we stop setting up the entities
-
-    _LOGGER.info(
-        (sensor_id, data, description)
-        for description in SENSOR_DESCRIPTIONS
-        for sensor_id, data in coordinator.data.items()
-        if description.exists_fn(data)
-    )
+    # first_device = next(iter(coordinator.data.values()))  # Get the first device in the dictionary
+    # _LOGGER.warning(f"First device SN: {first_device.serial_number}")
+    # _LOGGER.warning(f"First device history: {first_device.history}")
 
     async_add_entities(
-        IstaSensor(coordinator, sensor_id, data, description)
-        for description in SENSOR_DESCRIPTIONS
-        for sensor_id, data in coordinator.data.items()
-        if description.exists_fn(data)
+        (
+            IstaSensor(coordinator, serial_number, description)
+            for description in SENSOR_DESCRIPTIONS
+            for serial_number, device in coordinator.data.items()
+            if description.exists_fn(device)
+        )
     )
 
 
@@ -132,39 +128,130 @@ class IstaSensor(CoordinatorEntity[IstaCoordinator], SensorEntity):
     def __init__(
         self,
         coordinator: IstaCoordinator,
-        sensor_id: str,
-        data: str,
+        serial_number: str,
         entity_description,
     ) -> None:
         """Initialize the ista Calista sensor."""
         super().__init__(coordinator)
-        self.sensor_id = sensor_id
-
-        self._attr_unique_id = f"{sensor_id}_{entity_description.key}"
+        self.serial_number = serial_number
+        self._attr_unique_id = f"{serial_number}_{entity_description.key}"
         self.entity_description = entity_description
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
         return DeviceInfo(
-            identifiers={(DOMAIN, self.sensor_id)},
-            name=f"{self.coordinator.data[self.sensor_id]['tipo_equipo']} - {self.coordinator.data[self.sensor_id]['ubicacion']}",
+            identifiers={(DOMAIN, self.serial_number)},
+            name=f"{self.coordinator.data[self.serial_number].name}",
             manufacturer="ista SE",
             model="ista Calista",
-            model_id="TBD",
         )
 
     @property
     def native_value(self) -> StateType | datetime:
         """Return the state of the device."""
-        return self.entity_description.value_fn(self.coordinator.data[self.sensor_id])
+        return self.entity_description.value_fn(
+            self.coordinator.data[self.serial_number]
+        )
 
     async def async_added_to_hass(self) -> None:
         """When added to hass."""
         # perform initial statistics import when sensor is added, otherwise it would take
         # 1 day when _handle_coordinator_update is triggered for the first time.
+        if self.entity_description.generate_lts:
+            await self.update_statistics()
         await super().async_added_to_hass()
 
-    def _handle_coordinator_update(self) -> None:
+    async def _handle_coordinator_update(self) -> None:
         """Handle coordinator update."""
-        asyncio.run_coroutine_threadsafe(self.update_statistics(), self.hass.loop)
+        if self.entity_description.generate_lts:
+                    await self.update_statistics()
+
+    async def update_statistics(self) -> None:
+        """Import ista EcoTrend historical statistics."""
+
+        name = self.coordinator.config_entry.options.get(
+            f"lts_{self.entity_description.key}_{self.serial_number}"
+        )
+        if not name:
+            name = self.entity_id.removeprefix("sensor.")
+            self.hass.config_entries.async_update_entry(
+                entry=self.coordinator.config_entry,
+                options={
+                    **self.coordinator.config_entry.options,
+                    f"lts_{self.entity_description.key}_{self.serial_number}": name,
+                },
+            )
+
+        statistic_id = f"{DOMAIN}:{name}"
+
+        last_stats_sum = 0.0
+        last_stats_state = 0.0
+        last_stats_date = None
+        last_stats_last_reset = None
+        history = self.coordinator.data[self.serial_number].history
+
+        last_stats = await get_instance(self.hass).async_add_executor_job(
+            get_last_statistics,
+            self.hass,
+            1,
+            statistic_id,
+            False,
+            {"sum", "state", "last_reset"},
+        )
+
+        if last_stats:
+            last_stats_sum = last_stats[statistic_id][0].get("sum") or 0.0
+            last_stats_state = last_stats[statistic_id][0].get("state") or 0.0
+            last_stats_last_reset = datetime.fromtimestamp(
+                last_stats[statistic_id][0].get("last_reset") or 0, tz=UTC
+            )
+            last_stats_date = datetime.fromtimestamp(
+                last_stats[statistic_id][0].get("end") or 0, tz=UTC
+            ) + timedelta(days=1)
+
+        joined_history = []
+        # Loop through the list with the current and previous reading
+        for i in range(len(history)):
+            current_reading = history[i].reading
+
+            # If it's the first item, previous reading is 0
+            previous_reading = history[i - 1].reading if i > 0 else last_stats_state
+
+            if previous_reading > current_reading:
+                last_stats_last_reset = history[i].date
+                comsumption = 0
+            else:
+                comsumption = current_reading - previous_reading
+
+            last_stats_sum += comsumption
+            joined_history.append(
+                {
+                    "date": history[i].date,
+                    "current_reading": current_reading,
+                    "last_reset": last_stats_last_reset,
+                    "statistics_sum_diff": last_stats_sum,
+                }
+            )
+        statistics: list[StatisticData] = [
+            {
+                "start": history_data["date"],
+                "state": history_data["current_reading"],
+                "sum": history_data["statistics_sum_diff"],
+                "last_reset": history_data["last_reset"],
+            }
+            for history_data in joined_history
+            if last_stats_date is None or history_data["date"] > last_stats_date
+        ]
+
+        metadata: StatisticMetaData = {
+            "has_mean": False,
+            "has_sum": True,
+            "name": f"{self.device_entry.name} {self.name}",
+            "source": DOMAIN,
+            "statistic_id": statistic_id,
+            "unit_of_measurement": self.entity_description.native_unit_of_measurement,
+        }
+        if statistics:
+            _LOGGER.debug("Insert statistics: %s %s", metadata, statistics)
+            async_add_external_statistics(self.hass, metadata, statistics)
